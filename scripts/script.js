ae7b0704eb6b270b1979657ba2f28cabc950cd9f
iantheninja <munene.ian@live.com>
2020-09-09 14:40:08 +0300

    smooth scroll animations added to different page elements

--- scripts/script.js
+++ scripts/script.js
@@ -26,4 +26,36 @@ function linkAction(){
   navMenu.classList.remove('show')
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// scroll reveal animation
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+// scroll home
+sr.reveal('.home-title', {})
+sr.reveal('.button', {delay: 200})
+sr.reveal('.home-img', {delay: 400})
+sr.reveal('.home-social-icon', {interval: 200})
+
+// scroll about
+sr.reveal('.about-subtitle', {})
+sr.reveal('.about-img', {delay: 400})
+sr.reveal('.about-text', {interval: 200})
+
+// scroll skills
+sr.reveal('.skills-subtitle', {})
+sr.reveal('.skills-text', {delay: 200})
+sr.reveal('.skills-data', {interval: 200})
+sr.reveal('.skills-img', {delay: 200})
+
+// scroll work
+sr.reveal('.work-img', {interval: 200})
+
+// scroll contact
+sr.reveal('.contact-input', {interval: 200})
+sr.reveal('.contact-button', {delay: 400})
